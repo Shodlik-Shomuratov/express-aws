c4247d7585e65e7fc2cd9ff86ff66ec435c8a593
Shodlik Shomuratov <shodlikcoder@gmail.com>
2022-12-26 18:33:18 +0500

    First express app

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json({
+    status: 200,
+    message: 'Running...'
+  });
+});
+
+module.exports = router;
